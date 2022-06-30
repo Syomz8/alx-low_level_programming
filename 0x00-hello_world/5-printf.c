6a6580192bf1f948160e33d589939ac764b0b2fa
Syomz8 <msyomiti91@gmail.com>
2022-06-30 13:24:22 -0700

    kogf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
-*main-Entry point
-*Return: Always 0 (Success)
-*/
-int main(void) /* Description: 'the program's description' */
+ * main-Entrypoint
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main (void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0); } 
+return (0); }	
