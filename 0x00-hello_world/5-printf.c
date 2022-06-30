0e3188eec971cc7d54cca05b35fc2a6e6dc5393b
Syomz8 <msyomiti91@gmail.com>
2022-06-30 11:24:55 -0700

    using printf functon

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int main(void)/*Description: 'using printf function to output to std output'*/
+int main(void) /*Description: 'using printf function to output to std output'*/
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
