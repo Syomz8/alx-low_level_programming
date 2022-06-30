5a054655d6536c8ca45d018fb4ce170498fcf49a
Syomz8 <msyomiti91@gmail.com>
2022-06-30 09:26:24 -0700

    very tough toput it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-
-int main () {
-	char str1[52];
-
-	strcpy(str1, ""Programming is like building a multilingual puzzle")
-	
-	puts(str1);
-	
-	return(0);
-
-}	
-
-	
-
+int main()
+{
+   /*string initialisation */
+   char Mystr[] = ""Programming is like building a multilingual puzzle";
+  
+   puts(Mystr); /*writing the string to stdout*/
+   
+   return(0);
+}   
