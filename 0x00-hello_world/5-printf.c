1b17afe5dc6727d3a81b931bed2639f5e2843e1d
Syomz8 <msyomiti91@gmail.com>
2022-06-30 11:58:36 -0700

    gyh

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
  * functon main - Entry point
- * Description: 'using printf function to output to std output 
+ * Description: 'the program's description' 
  * Return: Always 0 (Success)
  */
 int main(void)
