0016df5a6f26ab1111bd62f0c5e746076e59e894
Syomz8 <msyomiti91@gmail.com>
2022-06-30 10:41:43 -0700

    gdrf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /*
  * main - Entry point
- * Description: 'using printf function to output to std output'
  * Return: Always 0 (Success)
  */
-int main(void)
+int main(void)/*Description: 'using printf function to output to std output'*/
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0); }	
