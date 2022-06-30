aa177a02cf775251ed6a4da0b4c044c3724c0b12
Syomz8 <msyomiti91@gmail.com>
2022-06-30 14:26:44 -0700

    pola

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char: %zu bytes(s)\n",sizeof((a));
-printf("Size of an int: %zu bytes(s)\n",sizeof((b));		
-printf("Size of a long int: %zu bytes(s)\n",sizeof((c));
-printf("Size of a long long int: %zu bytes(s)\n",sizeof((d));	
-printf("Size of a float: %zu bytes(s)\n",sizeof((e));
-return(0);
+printf("Size of a char: %zu bytes(s)\n",sizeof(a));
+printf("Size of an int: %zu bytes(s)\n",sizeof(b));		
+printf("Size of a long int: %zu bytes(s)\n",sizeof(c));
+printf("Size of a long long int: %zu bytes(s)\n",sizeof(d));	
+printf("Size of a float: %zu bytes(s)\n",sizeof(e));
+return (0);
 }
