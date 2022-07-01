21e4a211913f5e926b54a2e72d8e1fdf9f426814
Syomz8 <msyomiti91@gmail.com>
2022-07-01 07:21:51 -0700

    hexadecimao

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,10 @@ int main(void)
 	int num;
 
 	char letter;
+
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar('\n');
-	return(0);
+	return (0);
 }
