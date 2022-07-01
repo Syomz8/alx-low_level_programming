8ddc7a7e76e37b871769daa0888579f0340dc4b2
Syomz8 <msyomiti91@gmail.com>
2022-07-01 00:23:08 -0700

    size stuff

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main-Entrypoint to the program
- * Descriptio:'the program's description'
+ * Description:'the program's description'
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -12,10 +12,10 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char: %zu bytes(s)\n",sizeof(a));
-printf("Size of an int: %zu bytes(s)\n",sizeof(b));		
-printf("Size of a long int: %zu bytes(s)\n",sizeof(c));
-printf("Size of a long long int: %zu bytes(s)\n",sizeof(d));	
-printf("Size of a float: %zu bytes(s)\n",sizeof(e));
+printf("Size of a char: %zu bytes(s)\n", sizeof(a));
+printf("Size of an int: %zu bytes(s)\n", sizeof(b));		
+printf("Size of a long int: %zu bytes(s)\n", sizeof(c));
+printf("Size of a long long int: %zu bytes(s)\n", sizeof(d));	
+printf("Size of a float: %zu bytes(s)\n", sizeof(e));
 return (0);
 }
