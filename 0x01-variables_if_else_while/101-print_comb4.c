589ef4826d15f9a4aae16f499a1ec36f398fa02e
Syomz8 <msyomiti91@gmail.com>
2022-07-01 11:38:56 -0700

    come

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,10 +7,11 @@ int main(void)
 {
 	int c, i, k;
 
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
-		{	
+		{
 			for (k = '0'; k <= '9'; k++)
 			{
 				if (c < i && i < k)
@@ -29,7 +30,16 @@ int main(void)
 		}
 	}
 	
-	putchar('\n');
+	putchar ( '\n');
+
 	return (0);
 }	
+
+			
+
+				
+
+						
+
+
 
