eb4afbc9a9120e3ae7bc7f7516aa6015bb20e667
Syomz8 <msyomiti91@gmail.com>
2022-07-01 05:10:05 -0700

    pool

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Prints all single digit numbers of base 10 starting from 0, only using putchar and without char viarables
- * Return: Always 0
+ * main - Prints all single digit numbers of base 10 starting from 0 * Return: Always 0
  */
 int main(void)
 {
 	int num;
+
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 	putchar('\n');
