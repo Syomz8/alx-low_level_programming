7223d37dceede648e7c38a8ff04564013f08fa64
Syomz8 <msyomiti91@gmail.com>
2022-07-05 18:46:42 -0700

    kool

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - Adds two values
+* @f1: The first number to be computed
+* @f2: The second number to be computed
+* Return: Integer value
+*/
+int add(int f1, int f2)
+{
+return (f1 + f2);
+}	
