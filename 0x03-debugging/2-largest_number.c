dc7a96a84ff7d2f54ace47197aa3670f18e8ff51
Syomz8 <107145853+Syomz8@users.noreply.github.com>
2022-07-07 15:41:10 +0300

    Updating for task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -3,19 +3,20 @@
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
- *@b: second integer
- *@c: third integer
- *Return: largest number
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	
-	if (a >= b && a >= c)
+
+	if (a >= b && a > c)
 	{
-		largest = b;
+		largest = a;
 	}
-	else if (b >= a && b >= c)
+	else if (b >= a && b > c)
 	{
 		largest = b;
 	}
@@ -23,7 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
-	
-
