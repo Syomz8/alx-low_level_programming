fef5ed8ee82fa4ae0b1294af8f70906fc638502d
Syomz8 <msyomiti91@gmail.com>
2022-07-14 12:39:07 -0700

    tebu work wewe

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,9 @@
 #include "main.h"
 /**
  * _strcmp - Compares pointers to two strings.
- * @s1: A pointer to the first string to be compared.
- * @s2: A pointer to the second string to be compared.
- * Return: If str1 < str2, the negative difference of the first unmatched characters.
- *         If str1 == str2, 0.
- *         If str1 > str2, the positive difference of the first unmatched characters.
+ * @s1: input one.
+ * @s2: input two.
+ * Return: Always 0 (Success).
  */
 int _strcmp(char *s1, char *s2)
 {
