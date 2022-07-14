6724bb869b6818173be8a352ed786018cfb310f1
Syomz8 <107145853+Syomz8@users.noreply.github.com>
2022-07-14 21:57:16 +0300

    Fixing 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,7 +5,6 @@
  * @dest: The string to be appended upon.
  * @src: The string to be appended to dest.
  * @n: The number of bytes from src to be appended to dest.
- *
  * Return: A pointer to the resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
