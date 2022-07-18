b403c0f6ec60f65d66926bc13f05c618f5afa29e
Syomz8 <107145853+Syomz8@users.noreply.github.com>
2022-07-18 23:31:48 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,2 +1,16 @@
-nclude "main.h"
-#include <stdio.h>
+include "main.h"
+/**
+ * _memset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0 (Success)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
