7c29d3bfb8625370f3dd05eed18ec9bd85ddadf2
Syomz8 <107145853+Syomz8@users.noreply.github.com>
2022-07-18 23:45:57 +0300

    two

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,15 +2,17 @@ include "main.h"
 /**
  * _memset - Entry point
  * @s: pointed destination
- * @b: constant byte
+ * @c: constant byte
  * @n: bytes
  * Return: Always 0 (Success)
  */
-char *_memset(char *s, char b, unsigned int n)
+void *_memset(void *s, int c, size_t n)
 {
-	unsigned int i;
+	unsigned int index;
+	unsigned char *memory = s, value = c;
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
-	return (s);
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+
+	return (memory);
 }
