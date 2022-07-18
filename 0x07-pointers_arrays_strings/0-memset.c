7625fd03d95bf030b8b858199713fd2b9c93a8a3
Syomz8 <107145853+Syomz8@users.noreply.github.com>
2022-07-19 00:22:51 +0300

    7

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,16 @@
 include "main.h"
 /**
- * _memset - prints buffer in hexa
- * @s: buffer
- * @b: write
- * @n: size
- * Return: Nothing.
+ * _memset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0 (Success)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
-		*(s + i) = b;
-	}
+		s[i] = b;
 	return (s);
 }
