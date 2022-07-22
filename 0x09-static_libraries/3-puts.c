c1ce56aa5669e0a1246bc46d2826a7ea9bd05c8d
Syomz8 <107145853+Syomz8@users.noreply.github.com>
2022-07-23 00:37:02 +0300

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,1 +1,17 @@
-void _puts(char *s);
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
+ */
+void _puts(char *s)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
