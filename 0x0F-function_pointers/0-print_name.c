1d673acdc616b64fad67b43a060f629b44aa6b5d
Syomz8 <msyomiti91@gmail.com>
2022-08-03 12:19:09 -0700

    pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * print_name - prints the name
@@ -8,6 +7,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 		if (name == NULL || f == NULL)
-					return;
-			f(name);
+			return;
+			
+		f(name);
 }
