d543a80114d18aa878200cafbff823b67858d3fa
Syomz8 <107145853+Syomz8@users.noreply.github.com>
2022-08-11 18:08:22 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,25 +1,28 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * _strlen - lenght of string
- * @s:char
- * Return:int
+ * _strlen - finds the length of a string
+ * @str: string to find the length of
+ *
+ * Return: length of string
  */
-unsigned int _strlen(char *s)
+unsigned int _strlen(char *str)
 {
-	int i;
+	unsigned int i;
 
-		for (i = 0; s[i] != '\0'; i++)
-		{
-			continue;
-		}
-return (i);
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
 }
+
 /**
- * add_node - adding node
- * @head:last node list
- * @str:string
- * Return:new node created
+ * add_node - adds a node to the beginning of a linked list
+ * @head: double pointer to a lined list
+ * @str: string to add to the new node
+ *
+ * Return: pointer to the new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -27,19 +30,17 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (str == NULL)
 		return (NULL);
-new = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
-new->str = strdup(str);
+	new->str = strdup(str);
 	if (new->str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-new->len = _strlen(new->str);
-new->next = *head;
-*head = new;
-return (new);
+	new->len = _strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (new);
 }
