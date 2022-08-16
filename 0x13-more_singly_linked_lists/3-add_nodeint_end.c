c638f79b046ee1614f46130416e444601dd40bab
Syomz8 <107145853+Syomz8@users.noreply.github.com>
2022-08-16 22:49:42 +0300

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,7 +4,6 @@
  * add_nodeint_end - adds a node to the end of a linked list
  * @head: pointer to the head of the list
  * @n: number to be used as content
- *
  * Return: address of the newly added node
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
